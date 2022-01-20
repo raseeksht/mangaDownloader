3e2cb950c4e9564e1fef2b1890793ebe48421941
raseeksht <raseeksht1@gmail.com>
2022-01-20 18:27:02 +0545

    manga download init

---
+++ main.py
@@ -0,0 +1,187 @@
+import os
+from async_timeout import sys
+import requests
+from bs4 import BeautifulSoup
+import urllib.parse
+from PIL import Image
+import time
+
+tempImageDir = "temp_img"
+
+url = "https://mangaclash.com/?post_type=wp-manga&s="
+
+
+def getLastPartFromUrl(link):
+    if link.endswith("/"):
+        link = link[:-1]
+    return link.split("/")[-1]
+
+def getChapterNumber(link):
+    chapter = getLastPartFromUrl(link) #Example : gets "chapter-12-5"
+    chapter = chapter.split("chapter-")[-1]  # Removes "chapter-" -> Remaining "12-5"
+    if "-" in chapter: #means chapter value is in float
+        chapter = float(chapter.replace("-","."))
+    else:
+        chapter = int(chapter)
+    return chapter
+
+
+def askForNumber(maxNum,allowFloat=False):
+    if maxNum == 1:
+        print("only 1 choice is available.Choosing 1 by default..")
+        time.sleep(2)
+        return 1
+    while True:
+        try:
+            userChoice = input(f"choose 1 ... {maxNum} : ")
+            try:
+                userChoice = int(userChoice)
+            except Exception as e:
+                if allowFloat:
+                    userChoice = float(userChoice)
+                
+            # print(isinstance(userChoice,float))
+
+            if (userChoice > maxNum):
+                print('Unavailable Choice: ',userChoice)
+                continue
+            else:
+                return userChoice
+        except Exception as e:
+            print("alphabets and special symbol are not accepted...")
+
+def mangaclash():
+    searchTerm = input("Enter Manga to download: ")
+
+    searchUrl = url+urllib.parse.quote(searchTerm)
+
+    res = requests.get(searchUrl)
+
+    soup = BeautifulSoup(res.content,"html.parser")
+
+    noOfResult = soup.find('div',class_="tab-wrap").div.h1.text
+
+    print(noOfResult.strip())
+
+    # row c-tabs-item__content
+    mangasContainer = soup.find_all("div",{"class":"c-tabs-item__content"})
+    links = []
+    for index,manga in enumerate(mangasContainer):
+        mangaLink = manga.find("div",{"class":"post-title"}).h3.a
+        print(f"{index+1}. {mangaLink.text}")
+        links.append(mangaLink['href'])
+
+    # usrCh = int(input("Choose any: "))
+    print(noOfResult)
+    usrCh = askForNumber(len(mangasContainer))
+    return links[usrCh-1]
+
+
+def mangaClashSingleChapter(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content,"html.parser")
+    folderName = soup.find("h1",{"id":"chapter-heading"}).text
+    if not os.path.exists(tempImageDir):
+        os.mkdir(tempImageDir)
+    else:
+        os.chdir(tempImageDir)
+        os.system("rm *.*")
+        os.chdir("..")
+    if not os.path.exists("downloads"):
+        os.mkdir("downloads")
+    imgContainer = soup.find_all("div",{"class":"page-break"})
+    imgListForPdf = []
+    images = []
+    for imgDiv in imgContainer:
+        img = imgDiv.img
+        imgUrl = img['data-src'].strip()
+        imageName = imgUrl.split("/")[-1]
+        cmd = f"wget '{imgUrl}' -c -O '{tempImageDir}/{imageName}'"
+        os.system(cmd)
+        im = Image.open(f"{tempImageDir}/{imageName}")
+        im.convert('RGB')
+        imgListForPdf.append(im)
+    
+    imgListForPdf[0].save(f"./downloads/{folderName}.pdf",save_all=True,append_images=imgListForPdf[1:])
+    
+    os.system(f"rm {tempImageDir} -rf")
+    print("deleted temp image folder")
+
+
+def mangaClashDownload(link):
+    res = requests.get(link)
+    soup = BeautifulSoup(res.content,"html.parser")
+    ul = soup.find('ul',{"class":"version-chap"})
+    li = ul.find_all("li")[::-1]
+    liChapters = [getChapterNumber(li_elem.a['href']) for li_elem in li]
+    liChaptersUrls = [li_elem.a['href'] for li_elem in li]
+    liChaptersFloats = list(map(str,(filter(lambda x:isinstance(x,float),liChapters)))) #stores float chapters in list and converts float to str
+    print(f"{len(li)} Chapters available including chapters ({', '.join(liChaptersFloats)})")
+
+    print(f"\nLatest Chapter: {li[-1].a.text.strip()}\n")
+    
+    print("1. Download All")
+    print("2. Select a single Chapter")
+    print("3. from x to y")
+    usrCh = askForNumber(3)
+    if usrCh == 1:
+        for each in li:
+            a = each.a
+            chUrl = a['href']
+            print(f"Downloading {a.text}")
+            mangaClashSingleChapter(chUrl)
+    
+    elif usrCh == 2:
+        chapterChoice = askForNumber(getChapterNumber(li[-1].a['href']),allowFloat=True)
+        try:
+            chapterUrl = liChaptersUrls[liChapters.index(chapterChoice)]
+            mangaClashSingleChapter(chapterUrl)
+        except Exception:
+            print(f"Chapter {chapterChoice} is unavailable")
+            print("list of available chapters\n",liChapters)
+        
+    else:
+        while True:
+            print("from: ",end="")
+            frm = askForNumber(getChapterNumber(li[-1].a['href']),allowFloat=True)
+            print("to: ",end="")
+            to = askForNumber(getChapterNumber(li[-1].a['href']),allowFloat=True)
+            try:
+                indexOffrm = liChapters.index(frm)
+                indexOfto = liChapters.index(to)
+                break                
+            except Exception:
+                print(f"Chapter {frm} or {to} is unavailable")
+                print("list of available chapters\n",liChapters)
+                continue
+
+        # if user chooses frm = 5, to = 8 then let liChapters = [1,2,3,4,5,6,7.5,8,9,10]
+        # then index of 5 is 4 and index of 8 is 7
+        # now create new list liChapters[indexof(frm->5):indexof(to->8)+1]
+        # also create corresponding liChaptersUrls[(frm->5):indexof(to->8)+1]
+        # newChapters = liChapters[indexOffrm:indexOfto+1]
+        newChaptersUrls = liChaptersUrls[indexOffrm:indexOfto+1]
+
+        if frm > to:
+            print("Invalid Choice:")
+            print(f"From : {frm} ")
+            print(f"To   : {to}")
+            sys.exit()
+        elif frm == to: #download single chapter in this case
+            chapterUrl = liChaptersUrls[liChapters.index(chapterChoice)]
+            mangaClashSingleChapter(chapterUrl)
+        else:
+            for chapter in newChaptersUrls:
+                mangaClashSingleChapter(chapter)
+
+
+
+link = mangaclash()
+mangaClashDownload(link)
+
+# print(link)
+
+# print(a)
+
+
+# print(len(mangasContainer))
